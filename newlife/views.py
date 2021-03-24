aa38087e0e03992aecc81e55fde53342c798e77c
Abdumalik <axtrem13@gmail.com>
2021-03-24 18:55:50 +0500

    product listing in index page

--- newlife/views.py
+++ newlife/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.template import loader
 from news.models import News
+from sales.models import Product
+
 
 def index(request):
     news = News.objects.all()[:3]
+    products = Product.objects.all()
     context = {
-        'news': news
+        'news': news,
+        'products': products
     }
     return render(request, 'index.html', context)
